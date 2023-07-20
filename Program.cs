1570861549fbe87f14409ca618911e5e6d5a9adf
Anastasios-A <136964517+Anastasios-A@users.noreply.github.com>
2023-07-20 10:13:53 +0300

    var connection added
    var connection added + using sqlClient

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ReGenerationProjectAssignment_FundRaiser.DbContexts;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
